477ade9216eb9775713debcbddb16d45a2f985f2
maximka777 <maksim.borovskij@yandex.ru>
2019-07-08 19:08:43 +0300

    Refactoring tokenService

--- services/tokenService.js
+++ services/tokenService.js
@@ -1,24 +1,23 @@
 const ACCESS_TOKEN_KEY = 'ACCESS_TOKEN';
 
-function normalize(token) {
-  return token || '';
-}
-
 export default function(axios$, cookies) {
   // Initialization
-  axios$.setToken(normalize(cookies.get(ACCESS_TOKEN_KEY)));
+  const existingAccessToken = cookies.get(ACCESS_TOKEN_KEY);
+  if (existingAccessToken) {
+    axios$.setToken(existingAccessToken);
+  }
 
   return {
     getAccessToken() {
-      return normalize(cookies.get(ACCESS_TOKEN_KEY));
+      return cookies.get(ACCESS_TOKEN_KEY) || '';
     },
     setAccessToken(token) {
-      const normalizedToken = normalize(token);
-      cookies.set(ACCESS_TOKEN_KEY, normalizedToken);
-      axios$.setToken(normalizedToken);
+      cookies.set(ACCESS_TOKEN_KEY, token);
+      axios$.setToken(token);
     },
     clearAccessToken() {
-      this.setAccessToken('');
+      cookies.remove(ACCESS_TOKEN_KEY);
+      axios$.setToken(false);
     }
   }
 }
